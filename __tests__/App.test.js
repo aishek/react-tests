fe452fa9cadc578b36d9b722b391cf688a73131c
Alexandr Borisov <ab@cifronomika.ru>
2019-02-23 17:26:22 +0300

    simplified tests

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -17,28 +17,11 @@ it('switch tabs correctly', () => {
   expect(tree.render()).toMatchSnapshot()
 })
 
-it('have two tabs on render, first active', () => {
-  const tree = mount(<App/>)
-  // debugger
-
-  expect(tree).toIncludeText('Add tab')
-  expect(tree).toIncludeText('Title 1')
-  expect(tree).toIncludeText('Title 2')
-  expect(tree).toIncludeText('Content 1')
-  expect(tree).toIncludeText('Remove this tab (1)')
-  expect(tree).not.toIncludeText('Content 2')
-  expect(tree).not.toIncludeText('Remove this tab (2)')
-
-  const tabs = tree.find('[data-test="tab"]')
-  expect(tabs.length).toBe(2)
-
-  const tabContents = tree.find('[data-test="tab-content"]')
-  expect(tabContents.length).toBe(1)
-})
-
 it('remove first tab, second became active', () => {
   withStateLogger(() => {
     const tree = mount(<App/>)
+
+    // debugger
 
     expect(tree).toIncludeText('Title 1')
     expect(tree).toIncludeText('Title 2')
@@ -56,35 +39,18 @@ it('remove first tab, second became active', () => {
     expect(tree).not.toIncludeText('Remove this tab (1)')
     expect(tree).toIncludeText('Content 2')
     expect(tree).toIncludeText('Remove this tab (2)')
-    const tabs = tree.find('[data-test="tab"]')
-    expect(tabs.length).toBe(1)
-    const tabContents = tree.find('[data-test="tab-content"]')
-    expect(tabContents.length).toBe(1)
   })
 })
 
-it('add tab, activate just added', () => {
+it('add tab', () => {
   const tree = mount(<App/>)
 
   expect(tree).toIncludeText('Title 1')
   expect(tree).toIncludeText('Title 2')
   expect(tree).not.toIncludeText('Title 3')
-  expect(tree).toIncludeText('Content 1')
-  expect(tree).not.toIncludeText('Content 2')
-  const tabs = tree.find('[data-test="tab"]')
-  expect(tabs.length).toBe(2)
-  const tabContents = tree.find('[data-test="tab-content"]')
-  expect(tabContents.length).toBe(1)
 
   const addTabButton = tree.find('[data-test="add-tab"]')
   addTabButton.simulate('click')
 
   expect(tree).toIncludeText('Title 3')
-  const tabsAfterAdd = tree.find('[data-test="tab"]')
-  expect(tabsAfterAdd.length).toBe(3)
-  const justAddedTab = tabsAfterAdd.at(2)
-  justAddedTab.simulate('click')
-  expect(tree).not.toIncludeText('Content 1')
-  expect(tree).not.toIncludeText('Content 2')
-  expect(tree).toIncludeText('Content 3')
 })
